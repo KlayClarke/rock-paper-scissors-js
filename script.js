20450707da2200361c5bb5eec2e43fe1dccec8b8
Klay Clarke <klayaclarke@gmail.com>
2021-10-30 23:24:04 -0400

    updated and refactored code to allow for a better console experience for user

--- script.js
+++ script.js
@@ -1,68 +1,66 @@
-function game() {
-  let games = prompt("How many games would you like to play?");
-  let playerPoints = 0;
-  let computerPoints = 0;
+let playerName = prompt("What is your name?");
 
-  for (let i = 0; i < games; i++) {
-    let playerSelection = prompt(
-      "Please enter either rock, paper or scissors to begin"
-    ).toUpperCase();
-    let computerChoice = ["ROCK", "PAPER", "SCISSORS"];
-    let computerSelection =
-      computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    let round = playRound(playerSelection, computerSelection);
-    if (round === "WIN") {
-      playerPoints++;
-      console.log("You win");
-      console.log(playerPoints, computerPoints);
-    } else if (round === "LOSS") {
-      computerPoints++;
-      console.log("You lose");
-      console.log(playerPoints, computerPoints);
-    } else if (round === "TIE") {
-      console.log("TIED");
-      console.log(playerPoints, computerPoints);
-    }
-  }
+const choices = ["rock", "paper", "scissors"];
 
-  if (playerPoints < computerPoints) {
-    console.log(`The computer beat you ${computerPoints} to ${playerPoints}`);
-  } else if (playerPoints > computerPoints) {
-    console.log(
-      `Congratulations. You beat the computer ${playerPoints} to ${computerPoints} `
-    );
+function computerPlay() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+let playerSelection = choices[Math.floor(Math.random() * choices.length)];
+
+let computerSelection = computerPlay();
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return "lose";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "win";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "win";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "lose";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "lose";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "win";
   } else {
-    console.log(`The game ended in a draw`);
+    return "tied";
   }
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "ROCK") {
-      if (computerSelection == "ROCK") {
-        return "TIE";
-      } else if (computerSelection == "PAPER") {
-        return "LOSS";
-      } else if (computerSelection == "SCISSORS") {
-        return "WIN";
-      }
-    }
-    if (playerSelection == "PAPER") {
-      if (computerSelection == "PAPER") {
-        return "TIE";
-      } else if (computerSelection == "SCISSORS") {
-        return "LOSS";
-      } else if (computerSelection == "ROCK") {
-        return "WIN";
-      }
-    }
-    if (playerSelection == "SCISSORS") {
-      if (computerSelection == "ROCK") {
-        return "LOSS";
-      } else if (computerSelection == "PAPER") {
-        return "WIN";
-      } else if (computerSelection == "SCISSORS") {
-        return "TIE";
-      }
-    }
+}
+
+function gameResults() {
+  if (playerScore == computerScore) {
+    return "Game Results In Tie";
+  } else if (playerScore < computerScore) {
+    return `${playerName} loses`;
+  } else if (playerScore > computerScore) {
+    return `${playerName} wins`;
   }
+}
+
+function game() {
+  for (i = 0; i < 5; i++) {
+    let playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
+    let computerSelection = computerPlay();
+    let result = playRound(playerSelection, computerSelection);
+
+    if (result == "win") {
+      playerScore++;
+    } else if (result == "lose") {
+      computerScore++;
+    }
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    alert(`${playerName}: ${playerScore}; Computer: ${computerScore}`);
+    console.log(`${playerName}: ${playerScore}; Computer: ${computerScore}`);
+  }
+  alert(gameResults());
+  console.log(gameResults());
 }
 
 game();
